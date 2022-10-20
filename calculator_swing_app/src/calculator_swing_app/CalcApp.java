dcad00498d95f4035acc1ac6397b89de256bfd67
alex brito <alex.teixeira.brito@gmail.com>
2022-10-20 20:09:41 -0300

    Display criado

--- calculator_swing_app/src/calculator_swing_app/CalcApp.java
+++ calculator_swing_app/src/calculator_swing_app/CalcApp.java
@@ -3,10 +3,14 @@ package calculator_swing_app;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class CalcApp {
 
 	private JFrame fCalc;
+	private JTextField display;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -32,6 +36,13 @@ public class CalcApp {
 		fCalc.setBounds(100, 100, 300, 400);
 		fCalc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fCalc.getContentPane().setLayout(null);
+		
+		display = new JTextField();
+		display.setHorizontalAlignment(SwingConstants.RIGHT);
+		display.setFont(new Font("Dialog", Font.PLAIN, 40));
+		display.setText("0");
+		display.setBounds(12, 12, 274, 70);
+		fCalc.getContentPane().add(display);
+		display.setColumns(10);
 	}
-
 }
