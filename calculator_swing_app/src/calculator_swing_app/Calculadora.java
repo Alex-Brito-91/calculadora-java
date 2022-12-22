a01c2e0211618e6a49df651e9b817878d853a994
alex brito <alex.teixeira.brito@gmail.com>
2022-12-22 14:55:13 -0300

    Unificação das funções da calculadora

--- calculator_swing_app/src/calculator_swing_app/Calculadora.java
+++ calculator_swing_app/src/calculator_swing_app/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora extends JFrame {
 		getContentPane().setLayout(null);
 		setBackground(new Color(255, 255, 255));
 		setTitle("Calculadora");
-		setSize(300, 400);
+		setSize(300, 420);
 		setLocation(400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -19,17 +19,13 @@ public class Calculadora extends JFrame {
 		display.setBounds(12, 12, 274, 68);
 		getContentPane().add(display);
 		
-		Funcoes1 funcoes1 = new Funcoes1();
-		funcoes1.setBounds(12, 92, 274, 58);
-		getContentPane().add(funcoes1);
-		
-		Funcoes2 funcoes2 = new Funcoes2();
-		funcoes2.setBounds(214, 156, 69, 201);
-		getContentPane().add(funcoes2);
-		
 		Numeros numeros = new Numeros();
 		numeros.setBounds(15, 156, 193, 201);
 		getContentPane().add(numeros);
+		
+		Funcoes funcoes = new Funcoes();
+		funcoes.setBounds(12, 92, 274, 265);
+		getContentPane().add(funcoes);
 		
 		setVisible(true);
 		
--- calculator_swing_app/src/calculator_swing_app/Funcoes.java
+++ calculator_swing_app/src/calculator_swing_app/Funcoes.java
@@ -1,15 +1,14 @@
 package calculator_swing_app;
 
 import javax.swing.JLayeredPane;
-
 import javax.swing.JButton;
 
-public class Funcoes1 extends JLayeredPane {
+public class Funcoes extends JLayeredPane {
 	
-	public Funcoes1() {
+	public Funcoes() {
 		
 		JButton btnClear = new JButton("C");
-		btnClear.setBounds(5, 15, 55, 40);
+		btnClear.setBounds(7, 15, 53, 40);
 		add(btnClear);
 		
 		JButton btnPercent = new JButton("%");
@@ -23,6 +22,18 @@ public class Funcoes1 extends JLayeredPane {
 		JButton btnMult = new JButton("X");
 		btnMult.setBounds(206, 15, 55, 40);
 		add(btnMult);
+		
+		JButton btnAdd = new JButton("+");
+		btnAdd.setBounds(206, 76, 55, 50);
+		add(btnAdd);
+		
+		JButton btnSub = new JButton("-");
+		btnSub.setBounds(206, 132, 55, 50);
+		add(btnSub);
+		
+		JButton btnEquals = new JButton("=");
+		btnEquals.setBounds(206, 189, 55, 75);
+		add(btnEquals);
 		
 		setVisible(true);
 		
